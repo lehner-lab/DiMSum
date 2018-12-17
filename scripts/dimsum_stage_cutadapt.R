c27f841dffd8c2dc7bc23ab6c1cdb62ebfbc630d
andrefaure <a.jfaure@gmail.com>
2018-12-17 10:57:10 +0100

    fixed automatic linked adapters bug

--- scripts/dimsum_stage_cutadapt.R
+++ scripts/dimsum_stage_cutadapt.R
@@ -33,13 +33,15 @@ dimsum_stage_cutadapt <- function(
     if(!dimsum_meta[["transLibrary"]]){
       num_cut5f <- ifelse(is.na(dimsum_meta[['exp_design']][i,"cutadaptCut5First"]), 0, dimsum_meta[['exp_design']][i,"cutadaptCut5First"])
       num_cut5s <- ifelse(is.na(dimsum_meta[['exp_design']][i,"cutadaptCut5Second"]), 0, dimsum_meta[['exp_design']][i,"cutadaptCut5Second"])
+      num_cut3f <- ifelse(is.na(dimsum_meta[['exp_design']][i,"cutadaptCut3First"]), 0, dimsum_meta[['exp_design']][i,"cutadaptCut3First"])
+      num_cut3s <- ifelse(is.na(dimsum_meta[['exp_design']][i,"cutadaptCut3Second"]), 0, dimsum_meta[['exp_design']][i,"cutadaptCut3Second"])
       #Read 1
-      if(dimsum_meta[['exp_design']][i,"pair1_length"] > (num_cut5f + nchar(dimsum_meta[['exp_design']][i,"cutadapt5First"]) + nchar(dimsum_meta[['wildtypeSequence']]))){
+      if( (dimsum_meta[['exp_design']][i,"pair1_length"]-num_cut5f-num_cut3f) > (nchar(dimsum_meta[['exp_design']][i,"cutadapt5First"]) + nchar(dimsum_meta[['wildtypeSequence']])) ){
         dimsum_meta[['exp_design']][i,"cutadapt5First"] <- paste0(dimsum_meta[['exp_design']][i,"cutadapt5First"], "...", dimsum_meta[['exp_design']][i,"cutadapt3First"])
         dimsum_meta[['exp_design']][i,"cutadapt3First"] <- NA
       }
       #Read2
-      if(dimsum_meta[['exp_design']][i,"pair2_length"] > (num_cut5s + nchar(dimsum_meta[['exp_design']][i,"cutadapt5Second"]) + nchar(dimsum_meta[['wildtypeSequence']]))){
+      if( (dimsum_meta[['exp_design']][i,"pair2_length"]-num_cut5s-num_cut3s) > (nchar(dimsum_meta[['exp_design']][i,"cutadapt5Second"]) + nchar(dimsum_meta[['wildtypeSequence']])) ){
         dimsum_meta[['exp_design']][i,"cutadapt5Second"] <- paste0(dimsum_meta[['exp_design']][i,"cutadapt5Second"], "...", dimsum_meta[['exp_design']][i,"cutadapt3Second"])
         dimsum_meta[['exp_design']][i,"cutadapt3Second"] <- NA
       }
